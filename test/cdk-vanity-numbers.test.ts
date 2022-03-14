d7f9c012ee052fef05c775ebf03c2fea802f0c07
EC2 Default User <ec2-user@ip-172-31-49-190.ec2.internal>
2022-03-14 20:23:09 +0000

    Initial commit

---
+++ test/cdk-vanity-numbers.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkVanityNumbers from '../lib/cdk-vanity-numbers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-vanity-numbers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkVanityNumbers.CdkVanityNumbersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

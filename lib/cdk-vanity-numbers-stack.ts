28a0f1c843d009467e206eaa6dda78a88976009a
Vasil Urumov <vasil.urumov@fourth.com>
2022-03-14 23:41:54 +0200

    AddCDKVanity

--- lib/cdk-vanity-numbers-stack.ts
+++ lib/cdk-vanity-numbers-stack.ts
@@ -1,16 +1,39 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkVanityNumbersStack extends Stack {
+  //TODO: replace with existing Amazon Connect Instance ARN
+  private readonly callCenterARN = "AMAZON_CONNECT_INSTANCE_ARN";
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // define AWS DynamoDB table for vanity numbers
+    const table = new dynamodb.Table(this, 'VanityNumbersTable', {
+      partitionKey: { name: 'phoneNumber', type: dynamodb.AttributeType.STRING },
+    });
+    // define an AWS Lambda resource for create vanity numbers
+    const generateVanityNumber = new lambda.Function(this, 'GenerateVanityNumberHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda/generateVanityNumber-1cced3a7-3838-46ee-9b10-969667cafd30.zip"),
+      handler: 'index.handler',
+      environment: {
+        TABLE_NAME: table.tableName,
+        MIN_WORD_LENGTH: '3',
+        MAX_WORD_LENGTH: '12',
+        VANITY_NUMBER_COUNT: '5',
+        VANITY_NUMBER_MAX_WORD_COUNT: '1'
+      }
+    });
+    generateVanityNumber.node.addDependency(table);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkVanityNumbersQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // grant the lambda role read/write permissions to our table
+    table.grantReadWriteData(generateVanityNumber);
+
+    //TO DO define Amazon Connect ContactFlow for generating vanity numbers
+    
   }
 }
--- test/cdk-vanity-numbers.test.ts
+++ test/cdk-vanity-numbers.test.ts
@@ -1,17 +1,25 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkVanityNumbers from '../lib/cdk-vanity-numbers-stack';
+ import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as CdkVanityNumbers from '../lib/cdk-vanity-numbers-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-vanity-numbers-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkVanityNumbers.CdkVanityNumbersStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('Lambda Functions Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkVanityNumbers.CdkVanityNumbersStack(app, 'MyTestStack');
+  // THEN
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs("AWS::Lambda::Function", 2);
+});
+
+test('Contact Flow Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkVanityNumbers.CdkVanityNumbersStack(app, 'MyTestStack');
+  // THEN
+
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs("AWS::Connect::ContactFlow", 1);
 });
